620f661b8666a67d0d82ba7ac2e1acb062007b9e
Elsa Macasinag <99709576+macasinagelza@users.noreply.github.com>
2023-10-08 15:23:20 +0800

    with template

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
- 
+  <div class="container">
+    <h2>Two-Way Binding</h2>
+    <input v-model="message" placeholder="Enter a message" />
+     <p>Message: {{ message }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+};
+</script>
